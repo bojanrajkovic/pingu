55a3d9220853af304bdc11fafcc6ff851dc266ef
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2016-11-28 22:35:55 -0500

    Pingu: Add Benchmarking project
    This currently doesn't work because BenchmarkDotNet is
    broken for .NET Core RC3 tooling, but might as well add it
    anyway.

---
+++ src/Pingu.Benchmarks/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+
+using Pingu.Benchmarks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var switcher = new BenchmarkSwitcher(new[] {
+            typeof (Adler32Benchmark),
+        });
+
+        switcher.Run(args);
+    }
+}
