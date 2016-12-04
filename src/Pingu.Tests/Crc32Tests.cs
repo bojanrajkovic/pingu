0522d37e040bd7116820ba5381f78a5d234cf750
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2016-12-04 15:33:58 -0500

    Tests: Add some CRC32 tests.

---
+++ src/Pingu.Tests/Crc32Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pingu.Tests
+{
+    public class Crc32Tests
+    {
+        public static IEnumerable<object[]> Crc32TestVectors ()
+        {
+            byte[] ascii(string input) => Encoding.ASCII.GetBytes(input);
+            byte[] hex(string input) => Enumerable.Range(0, input.Length)
+                     .Where(x => x % 2 == 0)
+                     .Select(x => Convert.ToByte(input.Substring(x, 2), 16))
+                     .ToArray();
+
+            yield return new object[] { ascii("123456789"), 0xCBF43926 };
+            yield return new object[] { hex("49484452000002F0000005360806000000"), 0xF3B0825D };
+        }
+
+        [Theory]
+        [MemberData(nameof (Crc32TestVectors))]
+        public async Task Test_CRC32_Computation_Async(byte[] input, uint expected)
+        {
+            var actual = await Crc32.ComputeAsync(input);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
