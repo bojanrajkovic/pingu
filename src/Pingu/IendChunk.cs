7d9bedaecfc0253c5bf6d7a28c75b872a911a002
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2016-12-04 15:21:16 -0500

    Pingu: Add chunk implementations.

---
+++ src/Pingu/IendChunk.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Pingu
+{
+
+    public class IendChunk : Chunk
+    {
+        public override string Name => "IEND";
+        public override int Length => 0;
+
+        protected override Task<byte[]> GetChunkDataAsync() => Task.FromResult(Array.Empty<byte>());
+    }
+}
