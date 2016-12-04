a9a113825a2ac7df2ea8dbe08261989c054c62de
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2016-12-04 17:02:55 -0500

    Tests: Run pngcheck, check its output.

---
+++ src/Pingu.Tests/ProcessResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Pingu.Tests
+{
+    public class ProcessResult
+    {
+        public int ExitCode { get; internal set; }
+        public string StandardError { get; internal set; }
+        public string StandardOutput { get; internal set; }
+    }
+}
