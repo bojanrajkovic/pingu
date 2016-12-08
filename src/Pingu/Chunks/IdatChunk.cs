38f4992ec77e4ec04207e7ab72bbcfc6e60a0631
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2016-12-08 01:03:20 -0500

    Chunks: Move to namespace, fix nasty bug in IDAT
    The IDAT chunk was continuously clobbering the previous scanline
    when reading scanlines because I forgot that byte[]s are passed
    by reference and not by value. Thus, the read-filter-write loop
    in GetCompressedDataAsync would, at the beginning of every iteration,
    nicely read the next scanline in, and because we'd foolishly
    aliased `previousScanline` to `scanline`, we'd end up with both
    scanlines always being the same. This didn't present an issue to the
    Sub filter, but with the Up filter, it resulted in images that simply
    repeated the first scanline, ad nauseam.

--- src/Pingu/Chunks/IdatChunk.cs
+++ src/Pingu/Chunks/IdatChunk.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Pingu.Checksums;
 using Pingu.Filters;
 
-namespace Pingu
+namespace Pingu.Chunks
 {
 
     public class IdatChunk : Chunk
@@ -46,36 +46,41 @@ namespace Pingu
 
             int pixelWidth = GetPixelWidthForImage();
 
-            // Set up a couple of memory streams where we're going to hold our data.
-            // We need one wrapper stream over the raw data, a stream for the final data,
-            // and one for the 
-            MemoryStream readerStream = new MemoryStream(rawRgbData),
-                         compressedStream = new MemoryStream(),
-                         scanlineStream = new MemoryStream();
+            // Set up a couple of streams where we're going to hold our data.
+            // We need a stream for the final data and a deflate wrapper over it
+            var compressedStream = new MemoryStream();
+            var deflateStream = new DeflateStream(compressedStream, CompressionLevel.Optimal, true);
 
             // Write the zlib stream header. :-). 0x78 indicates DEFLATE compression (8) with
             // a sliding window of 2^7 (32 kB). 0xDA includes a check bit for the header and
             // an indication that we're using the optimal compression algorithm.
             compressedStream.Write(new byte[] { 0x78, 0xDA }, 0, 2);
 
+            var adler = new Adler32();
+
             // Apply filtering. Both byte[]s always represent the unfiltered scanline.
-            byte[] scanline = new byte[imageInfo.Width * pixelWidth],
-                   previousScanline = null;
+            byte[] previousScanline = null;
+            byte[] scanline = new byte[imageInfo.Width * pixelWidth];
             for (int i = 0; i < imageInfo.Height; i++) {
-                await readerStream.ReadAsync(scanline, 0, scanline.Length);
-                scanlineStream.WriteByte((byte) FilterType);
-                await scanlineStream.WriteAsync(Filter(scanline, previousScanline, pixelWidth), 0, scanline.Length);
-                previousScanline = scanline;
+                Buffer.BlockCopy(rawRgbData, i * scanline.Length, scanline, 0, scanline.Length);
+
+                // TODO: Replace IFilter API with FilterInto to avoid extra array allocation
+                var scanlineToWrite = new byte[1 + scanline.Length];
+                scanlineToWrite[0] = (byte) FilterType;
+
+                var filteredScanline = Filter(scanline, previousScanline, pixelWidth);
+                Buffer.BlockCopy(filteredScanline, 0, scanlineToWrite, 1, filteredScanline.Length);
+
+                adler.FeedBlock(scanlineToWrite);
+                
+                await deflateStream.WriteAsync(scanlineToWrite, 0, scanlineToWrite.Length);
+
+                previousScanline = previousScanline ?? new byte[scanline.Length];
+                Buffer.BlockCopy(scanline, 0, previousScanline, 0, scanline.Length);
             }
 
-            // Deflate the data and write it to the final compressed stream.
-            scanlineStream.Seek(0, SeekOrigin.Begin);
-            using (var ds = new DeflateStream(compressedStream, CompressionLevel.Optimal, true))
-                await scanlineStream.CopyToAsync(ds);
-
             // Write the ADLER32 Zlib checksum
-            var adler = Adler32.Compute(scanlineStream.ToArray());
-            var adlerBytes = GetBytesForInteger(adler);
+            var adlerBytes = GetBytesForInteger(adler.Hash);
             compressedStream.Write(adlerBytes, 0, adlerBytes.Length);
 
             // Store the compressed data and length.
