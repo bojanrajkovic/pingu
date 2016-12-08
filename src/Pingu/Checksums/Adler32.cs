3c7900a96a335fcec6e903f4ffad861ea30d4462
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2016-12-08 00:58:25 -0500

    Adler32: Support stepwise checksumming
    Convert Adler32 to an instance class and give it a
    FeedBlock method that simply applies the given block
    of bytes to the internal a and b states. Also add a public
    Hash property that completes the checksum by combining a
    and b.

--- src/Pingu/Checksums/Adler32.cs
+++ src/Pingu/Checksums/Adler32.cs
@@ -4,21 +4,27 @@ using System.Threading.Tasks;
 
 namespace Pingu.Checksums
 {
-    static class Adler32
+    class Adler32
     {
         const int Adler32Modulus = 65521;
 
-        public static unsafe int Compute(byte[] data)
+        uint a = 1, b = 0;
+
+        public int Hash => unchecked((int)((b << 16) | a));
+
+        public unsafe Adler32 FeedBlock(byte[] data)
         {
-            uint a = 1, b = 0;
             fixed (byte* ptr = data) {
                 for (var i = 0; i < data.Length; i++) {
                     a = (a + ptr[i]) % Adler32Modulus;
                     b = (b + a) % Adler32Modulus;
                 }
             }
-            return unchecked((int)((b << 16) | a));
+
+            return this;
         }
+
+        public static int Compute(byte[] data) => new Adler32().FeedBlock(data).Hash;
 
         public static async Task<int> ComputeAsync(Stream data)
         {
