51ab3dfec7573a2f6ba6873427d4e5a97d8a6c9a
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2016-12-11 00:49:41 -0500

    Benchmarks: Working benchmark suite!
    Includes Adler32 and Sub implementations. The benchmark
    suite currently works by having a "mirror" project.json
    for the benchmarks, and in order to run it, you must switch
    your .NET Core SDK to preview2. Once upstream updates, we'll
    hopefully be able to get rid of that and use the .csproj.
    
    Adler32 currently includes 4 implementations--a "smartest" that
    is close to upstream zlib's implementation, a "smarter" that
    does essentially what smartest does but without a number of unrolled
    16-byte loop operations first (it simply goes byte-by-byte),
    and two more naive implementations that go byte-by-byte without
    avoiding modulo math.
    
    Sub includes 3 implementations--a naive one with byte[]'s, a
    slightly optimized one that uses byte*'s, and an incredibly optimized
    implementation that uses vectors. On sizeable scanlines, this
    implementation is 10-15x faster than either of the other two, when
    run on RyuJIT. The legacy JIT and Mono are less good, so we may
    need to add some JIT-detection code to decide which Sub implementation
    we're going to use.

---
+++ src/Pingu.Benchmarks/Adler32Implementations.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Mathematics;
+using BenchmarkDotNet.Order;
+
+namespace Pingu.Benchmarks
+{
+    [OrderProvider(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn(NumeralSystem.Stars)]
+    [LegacyJitX64Job, RyuJitX64Job, MonoJob]
+    [Config(typeof(Config))]
+    public class Adler32Implementations
+    {
+        class Config : ManualConfig
+        {
+            public Config ()
+            {
+                Add(Job.Mono.With(Jit.Llvm).WithId("LlvmMono"));
+            }
+        }
+
+        Random rand = new Random();
+        byte[] data = new byte[77];
+
+        [Setup]
+        public void SetupData() => rand.NextBytes(data);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        unsafe void Do16(byte* buf, ref uint a, ref uint b) { Do8(buf, 0, ref a, ref b); Do8(buf, 8, ref a, ref b); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        unsafe void Do8(byte* buf, int i, ref uint a, ref uint b) { Do4(buf, i, ref a, ref b); Do4(buf, i + 4, ref a, ref b); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        unsafe void Do4(byte* buf, int i, ref uint a, ref uint b) { Do2(buf, i, ref a, ref b); Do2(buf, i + 2, ref a, ref b); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        unsafe void Do2(byte* buf, int i, ref uint a, ref uint b) { Do1(buf, i, ref a, ref b); Do1(buf, i + 1, ref a, ref b); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        unsafe void Do1(byte* buf, int i, ref uint a, ref uint b) { a += buf[i]; b += a; }
+
+        [Benchmark]
+        public unsafe int Smartest()
+        {
+            uint a = 1, b = 0;
+            const int nmax = 5552;
+
+            fixed (byte* ptr = data) {
+                byte* buf = ptr;
+                int len = data.Length, k;
+                while (len > 0) {
+                    k = len < nmax ? (int)len : nmax;
+                    len -= k;
+                    while (k >= 16) {
+                        Do16(buf, ref a, ref b);
+                        buf += 16;
+                        k -= 16;
+                    }
+                    if (k != 0) {
+                        do {
+                            a += *buf++;
+                            b += a;
+                        } while (--k > 0);
+                    }
+                    a %= 65521;
+                    b %= 65521;
+                }
+                return unchecked((int)((b << 16) | a));
+            }
+        }
+
+        [Benchmark]
+        public unsafe int Smarter()
+        {
+            uint a = 1, b = 0;
+            const int nmax = 5552;
+
+            fixed (byte* ptr = data) {
+                byte* buf = ptr;
+                int len = data.Length, k;
+                while (len > 0) {
+                    k = len < nmax ? (int)len : nmax;
+                    len -= k;
+                    if (k != 0) {
+                        do {
+                            a += *buf++;
+                            b += a;
+                        } while (--k > 0);
+                    }
+                    a %= 65521;
+                    b %= 65521;
+                }
+                return unchecked((int)((b << 16) | a));
+            }
+        }
+
+        [Benchmark]
+        public unsafe int UsePointer()
+        {
+            uint a = 1, b = 0;
+
+            fixed (byte* ptr = data) {
+                for (var i = 0; i < data.Length; i++) {
+                    a = (a + ptr[i]) % 65521;
+                    b = (b + a) % 65521;
+                }
+            }
+
+            return unchecked((int)((b << 16) | a));
+        }
+
+        [Benchmark(Baseline = true)]
+        public int NoPointer()
+        {
+            uint a = 1, b = 0;
+
+            for (var i = 0; i < data.Length; i++) {
+                a = (a + data[i]) % 65521;
+                b = (b + a) % 65521;
+            }
+
+            return unchecked((int)((b << 16) | a));
+        }
+    }
+}
---
+++ src/Pingu.Benchmarks/SubImplementations.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Numerics;
+using System.Security.Cryptography;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Mathematics;
+using BenchmarkDotNet.Order;
+
+namespace Pingu.Benchmarks
+{
+
+    [OrderProvider(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn(NumeralSystem.Stars)]
+    [LegacyJitX64Job, RyuJitX64Job, MonoJob]
+    public class SubImplementations
+    {
+        [Params(5000)]
+        public int TotalBytes { get; set; }
+
+        [Params(4)]
+        public int BytesPerPixel { get; set; }
+
+        byte[] data;
+
+        static readonly RandomNumberGenerator rng = RandomNumberGenerator.Create();
+
+        [Setup]
+        public void Setup()
+        {
+            data = new byte[TotalBytes];
+            rng.GetBytes(data);
+        }
+
+        [Benchmark]
+        public unsafe byte[] PointersOnly()
+        {
+            byte[] targetBuffer = new byte[data.Length];
+            fixed (byte* targetPtr = targetBuffer) {
+                fixed (byte* scanlinePtr = data) {
+                    Buffer.MemoryCopy(scanlinePtr, targetPtr, data.Length, BytesPerPixel);
+
+                    unchecked {
+                        // We start immediately after the first pixel--its bytes are unchanged. We only copied
+                        // bytesPerPixel bytes from the scanline, so we need to read over the raw scanline.
+                        for (var x = BytesPerPixel; x < data.Length; x++)
+                            targetPtr[x] = (byte)((scanlinePtr[x] - scanlinePtr[x - BytesPerPixel]) % 256);
+                    }
+                }
+            }
+            return targetBuffer;
+        }
+
+        [Benchmark(Baseline = true)]
+        public byte[] Naive()
+        {
+            byte[] targetBuffer = new byte[data.Length];
+            Buffer.BlockCopy(data, 0, targetBuffer, 0, BytesPerPixel);
+
+            unchecked {
+                // We start immediately after the first pixel--its bytes are unchanged. We only copied
+                // bytesPerPixel bytes from the scanline, so we need to read over the raw scanline.
+                for (var x = BytesPerPixel; x < data.Length; x++)
+                    targetBuffer[x] = (byte)((data[x] - data[x - BytesPerPixel]) % 256);
+            }
+
+            return targetBuffer;
+        }
+
+        [Benchmark]
+        public unsafe byte[] VectorAndPointer()
+        {
+            var vecSize = Vector<byte>.Count;
+            byte[] result = new byte[data.Length];
+
+            var chunks = (int)Math.Floor((float)(data.Length - BytesPerPixel) / vecSize);
+
+            fixed (byte* dataPtr = data) {
+                fixed (byte* resultPtr = result) {
+                    Buffer.MemoryCopy(dataPtr, resultPtr, data.Length, BytesPerPixel);
+
+                    for (int i = 0; i < chunks; i++) {
+                        int target = BytesPerPixel + i * vecSize;
+                        var vec = new Vector<byte>(data, target) - new Vector<byte>(data, i * vecSize);
+                        vec.CopyTo(result, target);
+                    }
+
+                    int start = (BytesPerPixel + (vecSize * chunks));
+                    for (int i = start; i < data.Length; i++)
+                        resultPtr[i] = unchecked((byte)(dataPtr[i] - dataPtr[i - BytesPerPixel]));
+                }
+            }
+
+            return result;
+        }
+    }
+}
