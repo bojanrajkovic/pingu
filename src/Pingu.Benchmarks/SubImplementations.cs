eb22b5999c3ae13a6400f6524596745265cb15cd
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2016-12-23 18:02:38 -0500

    Filters: Speed Sub filter by preoffsetting target
    Same optimization as for Up in the previous commit, along with a
    reshuffling of the benchmarks to be written more like the actual API itself.

--- src/Pingu.Benchmarks/SubImplementations.cs
+++ src/Pingu.Benchmarks/SubImplementations.cs
@@ -12,8 +12,8 @@ namespace Pingu.Benchmarks
 {
 
     [OrderProvider(SummaryOrderPolicy.FastestToSlowest)]
-    [RankColumn(NumeralSystem.Stars)]
-    [LegacyJitX64Job, RyuJitX64Job, MonoJob]
+    [RankColumn(NumeralSystem.Arabic)]
+    [RyuJitX64Job, LegacyJitX64Job, MonoJob]
     public class SubImplementations
     {
         [Params(5000)]
@@ -22,109 +22,138 @@ namespace Pingu.Benchmarks
         [Params(4)]
         public int BytesPerPixel { get; set; }
 
-        public byte[] Data { get; set; }
+        public byte[] RawScanline { get; set; }
+
+        public byte[] TargetBuffer { get; set; }
 
         static readonly RandomNumberGenerator rng = RandomNumberGenerator.Create();
 
         [Setup]
         public void Setup()
         {
-            Data = new byte[TotalBytes];
-            rng.GetBytes(Data);
+            RawScanline = new byte[TotalBytes];
+            TargetBuffer = new byte[TotalBytes];
+            rng.GetBytes(RawScanline);
         }
 
         [Benchmark]
-        public unsafe byte[] PointersOnly()
+        public unsafe void Pointers()
         {
-            byte[] targetBuffer = new byte[Data.Length];
-            fixed (byte* targetPtr = targetBuffer) {
-                fixed (byte* scanlinePtr = Data) {
-                    Buffer.MemoryCopy(scanlinePtr, targetPtr, Data.Length, BytesPerPixel);
+            int targetOffset = 0;
 
-                    unchecked {
-                        // We start immediately after the first pixel--its bytes are unchanged. We only copied
-                        // bytesPerPixel bytes from the scanline, so we need to read over the raw scanline.
-                        for (var x = BytesPerPixel; x < Data.Length; x++)
-                            targetPtr[x] = (byte)((scanlinePtr[x] - scanlinePtr[x - BytesPerPixel]) % 256);
-                    }
+            fixed (byte* targetPtr = TargetBuffer)
+            fixed (byte* scanlinePtr = RawScanline) {
+                Buffer.MemoryCopy(scanlinePtr, targetPtr + targetOffset, RawScanline.Length, BytesPerPixel);
+
+                unchecked {
+                    // We start immediately after the first pixel--its bytes are unchanged. We only copied
+                    // bytesPerPixel bytes from the scanline, so we need to read over the raw scanline.
+                    for (var x = BytesPerPixel; x < RawScanline.Length; x++)
+                        targetPtr[x + targetOffset] = (byte)((scanlinePtr[x] - scanlinePtr[x - BytesPerPixel]) % 256);
                 }
             }
-            return targetBuffer;
         }
 
         [Benchmark]
-        public unsafe byte[] PointersOnlyUnrolled()
+        public unsafe void PointersUnrolled()
         {
-            byte[] targetBuffer = new byte[Data.Length];
-            fixed (byte* targetPtr = targetBuffer) {
-                fixed (byte* scanlinePtr = Data) {
-                    Buffer.MemoryCopy(scanlinePtr, targetPtr, Data.Length, BytesPerPixel);
+            int targetOffset = 0;
 
-                    unchecked {
-                        // We start immediately after the first pixel--its bytes are unchanged. We only copied
-                        // bytesPerPixel bytes from the scanline, so we need to read over the raw scanline. Unroll
-                        // the loop a bit, as well.
-                        int x = BytesPerPixel;
-                        for (; Data.Length - x > 8; x += 8) {
-                            targetPtr[x] = (byte)((scanlinePtr[x] - scanlinePtr[x - BytesPerPixel]) % 256);
-                            targetPtr[x + 1] = (byte)((scanlinePtr[x + 1] - scanlinePtr[x + 1 - BytesPerPixel]) % 256);
-                            targetPtr[x + 2] = (byte)((scanlinePtr[x + 2] - scanlinePtr[x + 2 - BytesPerPixel]) % 256);
-                            targetPtr[x + 3] = (byte)((scanlinePtr[x + 3] - scanlinePtr[x + 3 - BytesPerPixel]) % 256);
-                            targetPtr[x + 4] = (byte)((scanlinePtr[x + 4] - scanlinePtr[x + 4 - BytesPerPixel]) % 256);
-                            targetPtr[x + 5] = (byte)((scanlinePtr[x + 5] - scanlinePtr[x + 5 - BytesPerPixel]) % 256);
-                            targetPtr[x + 6] = (byte)((scanlinePtr[x + 6] - scanlinePtr[x + 6 - BytesPerPixel]) % 256);
-                            targetPtr[x + 7] = (byte)((scanlinePtr[x + 7] - scanlinePtr[x + 7 - BytesPerPixel]) % 256);
-                        }
+            fixed (byte* target = TargetBuffer)
+            fixed (byte* raw = RawScanline) {
+                Buffer.MemoryCopy(raw, target + targetOffset, RawScanline.Length, BytesPerPixel);
 
-                        for (; x < Data.Length; x++)
-                            targetPtr[x] = (byte)((scanlinePtr[x] - scanlinePtr[x - BytesPerPixel]) % 256);
+                unchecked {
+                    // We start immediately after the first pixel--its bytes are unchanged. We only copied
+                    // bytesPerPixel bytes from the scanline, so we need to read over the raw scanline. Unroll
+                    // the loop a bit, as well.
+                    int x = BytesPerPixel;
+                    for (; RawScanline.Length - x > 8; x += 8) {
+                        target[x + targetOffset] = (byte)((raw[x] - raw[x - BytesPerPixel]) % 256);
+                        target[x + 1 + targetOffset] = (byte)((raw[x + 1] - raw[x + 1 - BytesPerPixel]) % 256);
+                        target[x + 2 + targetOffset] = (byte)((raw[x + 2] - raw[x + 2 - BytesPerPixel]) % 256);
+                        target[x + 3 + targetOffset] = (byte)((raw[x + 3] - raw[x + 3 - BytesPerPixel]) % 256);
+                        target[x + 4 + targetOffset] = (byte)((raw[x + 4] - raw[x + 4 - BytesPerPixel]) % 256);
+                        target[x + 5 + targetOffset] = (byte)((raw[x + 5] - raw[x + 5 - BytesPerPixel]) % 256);
+                        target[x + 6 + targetOffset] = (byte)((raw[x + 6] - raw[x + 6 - BytesPerPixel]) % 256);
+                        target[x + 7 + targetOffset] = (byte)((raw[x + 7] - raw[x + 7 - BytesPerPixel]) % 256);
                     }
+
+                    for (; x < RawScanline.Length; x++)
+                        target[x + targetOffset] = (byte)((raw[x] - raw[x - BytesPerPixel]) % 256);
                 }
             }
-            return targetBuffer;
+        }
+
+        [Benchmark]
+        public unsafe void PointersUnrolledPreOffset()
+        {
+            int targetOffset = 0;
+
+            fixed (byte* targetPreoffset = TargetBuffer)
+            fixed (byte* raw = RawScanline) {
+                byte* target = targetPreoffset + targetOffset;
+                Buffer.MemoryCopy(raw, target, RawScanline.Length, BytesPerPixel);
+
+                unchecked {
+                    // We start immediately after the first pixel--its bytes are unchanged. We only copied
+                    // bytesPerPixel bytes from the scanline, so we need to read over the raw scanline. Unroll
+                    // the loop a bit, as well.
+                    int x = BytesPerPixel;
+                    for (; RawScanline.Length - x > 8; x += 8) {
+                        target[x] = (byte)((raw[x] - raw[x - BytesPerPixel]) % 256);
+                        target[x + 1] = (byte)((raw[x + 1] - raw[x + 1 - BytesPerPixel]) % 256);
+                        target[x + 2] = (byte)((raw[x + 2] - raw[x + 2 - BytesPerPixel]) % 256);
+                        target[x + 3] = (byte)((raw[x + 3] - raw[x + 3 - BytesPerPixel]) % 256);
+                        target[x + 4] = (byte)((raw[x + 4] - raw[x + 4 - BytesPerPixel]) % 256);
+                        target[x + 5] = (byte)((raw[x + 5] - raw[x + 5 - BytesPerPixel]) % 256);
+                        target[x + 6] = (byte)((raw[x + 6] - raw[x + 6 - BytesPerPixel]) % 256);
+                        target[x + 7] = (byte)((raw[x + 7] - raw[x + 7 - BytesPerPixel]) % 256);
+                    }
+
+                    for (; x < RawScanline.Length; x++)
+                        target[x] = (byte)((raw[x] - raw[x - BytesPerPixel]) % 256);
+                }
+            }
         }
 
         [Benchmark(Baseline = true)]
-        public byte[] Naive()
+        public void Naive()
         {
-            byte[] targetBuffer = new byte[Data.Length];
-            Buffer.BlockCopy(Data, 0, targetBuffer, 0, BytesPerPixel);
+            int targetOffset = 0;
+            Buffer.BlockCopy(RawScanline, 0, TargetBuffer, targetOffset, BytesPerPixel);
 
             unchecked {
                 // We start immediately after the first pixel--its bytes are unchanged. We only copied
                 // bytesPerPixel bytes from the scanline, so we need to read over the raw scanline.
-                for (var x = BytesPerPixel; x < Data.Length; x++)
-                    targetBuffer[x] = (byte)((Data[x] - Data[x - BytesPerPixel]) % 256);
+                for (var x = BytesPerPixel; x < RawScanline.Length; x++)
+                    TargetBuffer[x + targetOffset] = (byte)((RawScanline[x] - RawScanline[x - BytesPerPixel]) % 256);
             }
-
-            return targetBuffer;
         }
 
         [Benchmark]
-        public unsafe byte[] VectorAndPointer()
+        public unsafe void VectorAndPointer()
         {
-            int vecSize = Vector<byte>.Count, length = Data.Length;
-            byte[] result = new byte[length];
+            int vecSize = Vector<byte>.Count, length = RawScanline.Length;
+            int targetOffset = 0;
 
             var chunks = (int)Math.Floor((double)(length - BytesPerPixel) / vecSize);
 
-            fixed (byte* dataPtr = Data) {
-                fixed (byte* resultPtr = result) {
-                    Buffer.MemoryCopy(dataPtr, resultPtr, length, BytesPerPixel);
+            fixed (byte* raw = RawScanline) {
+                fixed (byte* target = TargetBuffer) {
+                    Buffer.MemoryCopy(raw, target + targetOffset, length, BytesPerPixel);
 
                     for (int i = 0; i < chunks; i++) {
                         int src = i * vecSize, dst = src + BytesPerPixel;
-                        var vec = new Vector<byte>(Data, dst) - new Vector<byte>(Data, src);
-                        vec.CopyTo(result, dst);
+                        var vec = new Vector<byte>(RawScanline, dst) - new Vector<byte>(RawScanline, src);
+                        vec.CopyTo(TargetBuffer, dst + targetOffset);
                     }
 
-                    int start = BytesPerPixel + (vecSize * chunks);
+                    int start = BytesPerPixel + (vecSize * chunks) + targetOffset;
                     for (int i = start; i < length; i++)
-                        resultPtr[i] = unchecked((byte)(dataPtr[i] - dataPtr[i - BytesPerPixel]));
+                        target[i] = unchecked((byte)(raw[i] - raw[i - BytesPerPixel]));
                 }
             }
-
-            return result;
         }
     }
 }
