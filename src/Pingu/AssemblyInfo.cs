f8bdf4485b66c3336bbb796fff7005f87c7d98c4
Bojan Rajkovic <bojan.rajkovic@xamarin.com>
2016-12-25 21:58:31 -0800

    CI: AppVeyor CI (#12)
    Add CI with AppVeyor.

--- src/Pingu/AssemblyInfo.cs
+++ src/Pingu/AssemblyInfo.cs
@@ -1,4 +1,10 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Reflection;
 
 [assembly: InternalsVisibleTo("Pingu.Tests")]
 [assembly: InternalsVisibleTo("Pingu.Benchmarks")]
+
+[assembly: AssemblyCompany("Code, Rinse, Repeat")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
