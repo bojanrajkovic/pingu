392c3c8a48e58b33caaea2ea0fa1d85fef3ba863
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2016-12-26 00:18:39 -0500

    CI: Set some assembly info attributes for patching.

--- src/Pingu/AssemblyInfo.cs
+++ src/Pingu/AssemblyInfo.cs
@@ -1,4 +1,14 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Pingu.Tests")]
 [assembly: InternalsVisibleTo("Pingu.Benchmarks")]
+
+[assembly: AssemblyCompany("Code, Rinse, Repeat")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("Pingu")]
+[assembly: AssemblyTitle("Pingu")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
