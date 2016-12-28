d66ad765e3493229fb2ce78dfaa065dbedd1255b
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2016-12-28 15:10:16 -0500

    Pingu: Add a pallette and transparency map, with tests
    Add a pallette and transparency map as part of implementing image
    sources, starting with indexed images.

---
+++ src/Pingu/Colors/Pallette.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+namespace Pingu.Colors
+{
+    public class Pallette
+    {
+        int[] palletteEntries;
+        Dictionary<int, int> reversePallette;
+
+        int currentPalletteEntry = 0;
+
+        public int PalletteSize { get; }
+
+        public Pallette(int bitDepth)
+        {
+            // The pallette size must not be larger than 2^bitDepth entries.
+            PalletteSize = 2 << (bitDepth-1);
+            palletteEntries = new int[PalletteSize];
+            reversePallette = new Dictionary<int, int> (PalletteSize);
+        }
+
+        internal int AddColorToPallette(byte r, byte g, byte b)
+        {
+            if (currentPalletteEntry == PalletteSize)
+                throw new Exception($"Pallette is at capacity, increase bit depth for the image.");
+
+            int color = ColorHelpers.PackRgb(r, g, b);
+            palletteEntries[currentPalletteEntry] = color;
+            reversePallette[color] = currentPalletteEntry;
+
+            return currentPalletteEntry++;
+        }
+
+        internal int GetPackedColor(int i) => palletteEntries[i];
+
+        public byte[] this[int i] => ColorHelpers.UnpackRgb(palletteEntries[i]);
+
+        public int this[byte r, byte g, byte b]
+        {
+            get {
+                var color = r << 16 | g << 8 | b;
+                return reversePallette[color];
+            }
+        }
+    }
+}
