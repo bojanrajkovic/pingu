a70cc5fae24885f4a158f8e6fa8e1e832b6d869e
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2016-12-29 14:18:54 -0500

    Math: Add fast ceiling and byte down-quantization functions.

---
+++ src/Pingu/Math/Ceil.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Pingu
+{
+    static partial class Math
+    {
+        const double doubleMagicDelta = (1.5e-8);
+        const double doubleMagicRoundEps = (.5f - doubleMagicDelta);
+        const double doubleMagic = 6755399441055744.0;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe int Ceil(double val)
+        {
+            val = val + doubleMagicRoundEps + doubleMagic;
+            return ((int*)&val)[0];
+        }
+    }
+}
---
+++ src/Pingu/Math/Reduce.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Pingu
+{
+    static partial class Math
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static byte Reduce(byte gray, int targetBits) => (byte)(((float)gray / 256) * (2 << (targetBits - 1)));
+    }
+}
