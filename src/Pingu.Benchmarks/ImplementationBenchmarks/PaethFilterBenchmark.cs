3cca2552bec49f9fd4f193e6241a7793aed14511
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2017-06-19 00:19:35 -0400

    Benchmarks: Add Paeth implementation benchmark.

---
+++ src/Pingu.Benchmarks/ImplementationBenchmarks/PaethFilterBenchmark.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace Pingu.Benchmarks.ImplementationBenchmarks
+{
+    [Config(typeof(Config))]
+    [OrderProvider(SummaryOrderPolicy.FastestToSlowest)]
+    public class PaethFilterBenchmark
+    {
+        const int TotalBytes = 5000;
+        byte[] targetBuffer, rawScanline, previousScanline;
+
+        static readonly RandomNumberGenerator rng = RandomNumberGenerator.Create();
+
+        [Params(3, 4)]
+        public int BytesPerPixel { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            targetBuffer = new byte[TotalBytes];
+            rawScanline = new byte[TotalBytes];
+            previousScanline = new byte[TotalBytes];
+            rng.GetBytes(rawScanline);
+            rng.GetBytes(previousScanline);
+        }
+
+        [Benchmark(Baseline = true)]
+        public void PinguSubFilterPointers()
+        {
+            var filter = Filters.PaethFilter.Instance;
+            filter.FilterInto(targetBuffer, 0, rawScanline, previousScanline, BytesPerPixel);
+        }
+    }
+}
